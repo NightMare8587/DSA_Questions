7d8f87d0dd06af23ff90aef109453e0144bba9da
Pulkit <pulkit.loya@myn.global>
2024-08-19 16:26:18 +0530

    implemented optimal solution

--- src/arrays/BuyAndSellStocks.java
+++ src/arrays/BuyAndSellStocks.java
@@ -2,10 +2,10 @@ package arrays;
 
 public class BuyAndSellStocks {
     public static void main(String[] args) {
-        int[] prices = {10,1,5,6,7,1};
+        int[] prices = {10,1,5,6,7};
         int arraySize = prices.length;
 
-        int maxProfit = searchAndFindMaxProfitBruteForce(prices,arraySize);
+        int maxProfit = searchAndFindMaxProfitBetterSolution(prices,arraySize);
 
         System.out.println("Max Profit in given prices is: " + maxProfit);
     }
@@ -16,12 +16,36 @@ public class BuyAndSellStocks {
         int MAX_PROFIT = 0;
 
         for(int i=0; i<arraySize-1; i++) {
+            //outer loop will have buying price of stock
             int currentPrice = prices[i];
             for(int j = i+1; j<arraySize; j++) {
+                // if buy price is higher than other days then skip that
                 if(currentPrice > prices[j])
                     continue;
 
+                // get the max profit
                 MAX_PROFIT = Math.max(prices[j] - currentPrice,MAX_PROFIT);
+            }
+        }
+
+        return MAX_PROFIT;
+    }
+
+    // time complexity O(n)
+    // Space Complexity O(1)
+    static int searchAndFindMaxProfitBetterSolution(int[] prices, int arraySize) {
+        int MAX_PROFIT = 0;
+        //initial price i.e. buying on day 1
+        int buyPrice = prices[0];
+
+        for(int i=1; i<arraySize; i++) {
+            // if there is lower value than buying price
+            // then buy that stock and update the buyPrice
+            if(buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            } else {
+                // calculate the max profit
+                MAX_PROFIT = Math.max(prices[i] - buyPrice, MAX_PROFIT);
             }
         }
 
